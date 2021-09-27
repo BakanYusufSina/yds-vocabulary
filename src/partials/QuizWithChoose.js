61f480457392de555fa2283bad5daa356eccd8d9
BakanYusufSina <BakanYusufSina@users.noreply.github.com>
2021-09-27 15:14:34 +0300

    270921 quiz updates

--- src/partials/QuizWithChoose.js
+++ src/partials/QuizWithChoose.js
@@ -3,48 +3,54 @@ import { View, Text, StyleSheet } from 'react-native'
 import { openDatabase } from 'react-native-sqlite-storage'
 
 export default function QuizWithChoose(props) {
-    const [quizAnswers, setQuizAnswers] = useState(null)
+    const [quizAnswers, setQuizAnswers] = useState([]) //Random answer list
     const [questionCount, setCount] = useState(props.questionCount)
-    const [questions, setQuestions] = useState([...props.questions])
-    const [questionAnswers, setQuestionAnswers] = useState([])
+    const [questions, setQuestions] = useState([...props.questions])//All questions
+    const [questionAnswers, setQuestionAnswers] = useState([])//Answers for one question
     const db = openDatabase({
         name: 'yds',
         createFromLocation: '~www/sqlite_yds.db'
     })
     useEffect(() => {
         db.transaction(tx => {
-            tx.executeSql('SELECT translate FROM dictionary ORDER BY random() LIMIT ?',
-                [props.questionCount * 5],
+            tx.executeSql('SELECT translate FROM dictionary ORDER BY random() LIMIT 10',
+                [],
                 async (tx, results) => {
                     const answers = []
-                    if (results.rowsAffected > 0) {
-                        for (let i = 0; i < results.rows.item.length; i++)
-                            await answers.push(results.rows.item(i))
-                        console.log('answers', answers);
-                    }
+                    for (let i = 0; i < results.rows.length; i++)
+                        await answers.push(results.rows.item(i).translate)
                     setQuizAnswers(answers)
                 })
         })
     }, [])
-    const getAnswerList = async (qIndex) => {
+    const getAnswerList = (qIndex) => {
         const answers = []
+        const indexList = []
+        for (let i = 0; indexList.length < 3; i++)
+            if (indexList.indexOf(quizAnswers[Math.floor(Math.random() * quizAnswers.length)]) == -1)
+                indexList.push(quizAnswers[Math.floor(Math.random() * quizAnswers.length)])
         for (let i = 0; i <= 3; i++) {
             if (i == 3)
-                await answers.push(questions[qIndex].translate)
+                answers.push(questions[qIndex].translate)
             else
-                await answers.push(quizAnswers[Math.floor(Math.random() * i + (questionCount - 1))])
+                answers.push(indexList[i])
         }
-        setQuestionAnswers(answers)
+        console.log(answers);
+        //setQuestionAnswers(answers)
     }
-    if (questions.length === 0) return null
+    if (questions.length === 0 && quizAnswers.length === 0) return null
     return (
         <View style={styles.container}>
+            {getAnswerList(0)}
             <View style={styles.questionContainer}>
                 <Text style={styles.questionText}>
                     {questions[0].vocabulary}
                 </Text>
             </View>
-
+            {questionAnswers.map((l, i) =>
+                <View style={styles.answersContainer} key={i}>
+                </View>
+            )}
         </View>
     )
 }
